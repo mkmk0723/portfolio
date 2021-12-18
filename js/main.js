53530db6159aa6fc6b32aa2379fbf8912b9d6f75
mkmk0723 <mkmk0723@gmail.com>
2021-12-18 17:04:10 +0900

    更新

--- js/main.js
+++ js/main.js
@@ -50,5 +50,30 @@ $(function(){
     });
 
 
+    /*-------------site-titleアニメーション--------------------------------*/ 
+var container = $(".site-title");
+// アニメーションのスピード
+var speed = 80;
+
+// テキストの間にスペースを入れる
+var content = container.html();
+var text = $.trim(content);
+var newHtml = "";
+
+// スペースで区切ったテキストを、テキストの数だけspanで囲む
+text.split("").forEach(function(v){
+    newHtml += '<span>' + v + '<span>';
+});
+
+// spanで囲んだできストをHTMLに戻す
+container.html(newHtml);
+
+// １文字ずつ表示
+var txtNum = 0;
+container.css({opacity:1});
+setInterval(function(){
+    container.find('span').eq(txtNum).css({opacity:1});  
+txtNum++
+},speed);
 });
 
